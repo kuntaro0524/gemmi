5512c3d68a881663b66bc182abf8f91ddd6284f6
平田邦生 <kuntaro@vu034.spring8.or.jp>
2022-12-19 09:57:45 +0900

    反射ファイルを読み込んでCCを順番に計算する

--- 01.mtz_cc/read_mtz_and_cc.py
+++ 01.mtz_cc/read_mtz_and_cc.py
@@ -6,7 +6,8 @@ from matplotlib import pyplot as plt
 
 mtz = gemmi.read_mtz_file(sys.argv[1])
 
-print(mtz.cell)
+cell1 = mtz.cell
+print(cell1.a,cell1.b,cell1.c)
 print(mtz.spacegroup)
 print(mtz.title)
 print(mtz.history)
@@ -18,36 +19,44 @@ print(mtz.max_1_d2)
 imean=mtz.get_float("IMEAN")
 
 # make DataFrame from MTZ file
-mtz = gemmi.read_mtz_file(sys.argv[1])
 mtz_data = np.array(mtz, copy=False)
 mtz_df = pd.DataFrame(data=mtz_data, columns=mtz.column_labels())
 # (optional) store Miller indices as integers
 mtz_df1 = mtz_df.astype({label: 'int32' for label in 'HKL'})
-print(mtz.cell)
-
-#print(mtz_df1.describe())
+cell1 = mtz.cell
+print(cell1.a,cell1.b,cell1.c)
 
 # the second mtz file
 mtz2 = gemmi.read_mtz_file(sys.argv[2])
-print(mtz2.cell)
+cell2 = mtz2.cell
 mtz_data2 = np.array(mtz2, copy=False)
 mtz_df2 = pd.DataFrame(data=mtz_data2, columns=mtz2.column_labels())
 # (optional) store Miller indices as integers
 mtz_df2 = mtz_df2.astype({label: 'int32' for label in 'HKL'})
 mtz_df2_new=mtz_df2.rename(columns={"IMEAN": "IMEAN2"})
-print(mtz_df2.columns)
-#print(mtz_df2_new.describe())
 
 # merge DataFrames
 df = pd.merge(mtz_df1, mtz_df2_new, on=['H', 'K', 'L'])
+df_cc=df.corr()
 
-print(df.describe())
+# Correlation coefficient of 
+cc=df['IMEAN'].corr(df['IMEAN2'])
+
+comment1="%8.3f%8.3f%8.3f(%s)\n"% (cell1.a,cell1.b,cell1.c,sys.argv[1])
+comment2="%8.3f%8.3f%8.3f(%s)\n"% (cell2.a,cell2.b,cell2.c,sys.argv[2])
+
+cell_diff = cell1.b-cell2.b
+comment3="CC=%8.3f CellDiff=%8.3f"%(cc, cell_diff)
+
+title_s=comment1+comment2+comment3
 
 # plot FP from MTZ as a function of F_meas_au from mmCIF
-plt.rc('font', size=8)
-plt.figure(figsize=(2, 2))
-plt.scatter(x=df['IMEAN'], y=df['IMEAN2'],
-               marker=',', s=1, linewidths=0)
+plt.rc('font', size=14)
+plt.figure(figsize=(8, 8))
+plt.scatter(x=df['IMEAN'], y=df['IMEAN2'], marker=',', s=1, linewidths=0)
+plt.title(title_s)
 plt.xlim(xmin=0)
 plt.ylim(ymin=0)
-plt.show()
+plt.savefig("test.png")
+#plt.show()
+
